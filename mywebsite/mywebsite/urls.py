0413b84b2c6cbb9611cb4cf73afa66f3bb78c301
Viraj Wadate <42370583+VirajWadate@users.noreply.github.com>
2023-09-17 12:18:25 +0530

    Update urls.py

--- mywebsite/mywebsite/urls.py
+++ mywebsite/mywebsite/urls.py
@@ -17,9 +17,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
-from .views import index
+from .views import index, test
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', index)
+    path('', index),
+    path('test', test)
 ]
